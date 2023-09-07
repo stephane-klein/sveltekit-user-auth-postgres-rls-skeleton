2876e713d0b2ba94c91513438f62733b1e5fd5fb
Stéphane Klein <contact@stephane-klein.info>
2023-09-07 17:49:37 +0200

    Display message in user isn't authorize to access to page

--- src/routes/(private)/+layout.server.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-export async function load({ locals }) {
-    if (!locals?.user) {
-        throw redirect(302, "/");
-    }
-}
---
+++ src/routes/(private)/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data;
+</script>
+
+{#if data?.user}
+    <slot />
+{:else}
+    <p>This page does not exist or you are not authorized to access it.</p>
+
+    <p>
+        Go to <a href="/">home page...</a>
+    </p>
+{/if}
