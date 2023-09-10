b60165b9eba2fb77ea5cbbc3428295dc63a6ea10
Stéphane Klein <contact@stephane-klein.info>
2023-09-10 15:04:18 +0200

    WIP

--- tests/rls_main.js
+++ tests/rls_main.js
@@ -2,19 +2,24 @@ import postgres from "postgres";
 import fixture from "../load-fixtures.js";
 
 let sql;
+let sqlFixture;
 
 beforeEach(async() => {
     sql = postgres(
         "postgres://webapp:password@localhost:5433/myapp"
     );
+    sqlFixture = postgres(
+        "postgres://postgrestest:passwordtest@localhost:5433/myapp"
+    );
 });
 afterEach(() => {
     sql.end();
+    sqlFixture.end();
 });
 
 describe("When john-doe2 user request the list of resource_a", () => {
     it("Should return 3 resource_a", async() => {
-        await fixture(sql);
+        await fixture(sqlFixture);
         const result = await sql.begin((sql) => [
             sql`SELECT auth.open_session(
                     (SELECT auth.authenticate(
