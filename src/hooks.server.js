60697f68dddfbe8f5d4df1c0a49cb45de36a9d6e
Stéphane Klein <contact@stephane-klein.info>
2023-09-11 15:34:44 +0200

    WIP

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -10,92 +10,40 @@ export async function handle({ event, resolve }) {
     const sessionId = event.cookies.get("session");
 
     if (sessionId) {
-        await event.locals.sql`
+        const openSessionResult = (await event.locals.sql`
             SELECT auth.open_session(${sessionId});
-        `;
-        try {
-            const rows = await event.locals.sql`
-                WITH _user AS (
-                    SELECT
-                        users.id,
-                        users.username,
-                        users.first_name,
-                        users.last_name,
-                        users.email,
-                        users.is_active,
-                        users.last_login,
-                        users.date_joined,
-                        users.created_at,
-                        users.updated_at
-                    FROM
-                        auth.sessions
-                    INNER JOIN auth.users
-                        ON sessions.user_id = users.id
-                    WHERE
-                        sessions.id=${sessionId}
-                    LIMIT 1
-                ),
-                _impersonate_user AS (
-                    SELECT
-                        users.id,
-                        users.username,
-                        users.first_name,
-                        users.last_name,
-                        users.email,
-                        users.is_active,
-                        users.last_login,
-                        users.date_joined,
-                        users.created_at,
-                        users.updated_at
-                    FROM
-                        auth.sessions
-                    INNER JOIN auth.users
-                        ON sessions.impersonate_user_id = users.id
-                    WHERE
-                        sessions.id=${sessionId}
-                    LIMIT 1
-                ),
-                _spaces AS (
-                    SELECT
-                        spaces.id        AS id,
-                        spaces.slug      AS slug,
-                        spaces.title     AS title,
-                        space_users.role AS role
-                    FROM _user
-                    LEFT JOIN auth.space_users
-                           ON space_users.user_id=_user.id
-                    LEFT JOIN auth.spaces
-                           ON space_users.space_id=spaces.id
-                ),
-                _current_space AS (
-                    SELECT *
-                    FROM _spaces
-                    WHERE _spaces.slug=${event.params?.space_slug || null}
-                    LIMIT 1
-                )
+        `)[0].open_session;
+
+        event.locals.session_id = sessionId;
+        event.locals.client.user = openSessionResult.user;
+        event.locals.client.impersonated_by = openSessionResult.impersonated_by;
+
+        const result = (await event.locals.sql`
+            WITH _spaces AS (
                 SELECT
-                    (SELECT ROW_TO_JSON(_user) FROM _user) AS user,
-                    (SELECT ROW_TO_JSON(_impersonate_user) FROM _impersonate_user) AS impersonate_user,
-                    (SELECT ARRAY_AGG(ROW_TO_JSON(_spaces)) FROM _spaces)::JSONB[] AS spaces,
-                    (SELECT ROW_TO_JSON(_current_space) FROM _current_space) AS current_space
-            `;
-            if (rows?.length > 0) {
-                event.locals.session_id = sessionId;
-                if (rows[0].impersonate_user === null) {
-                    event.locals.client.impersonated = false;
-                    event.locals.client.user = rows[0].user;
-                    event.locals.client.impersonated_by = null;
-                } else {
-                    event.locals.client.impersonated = true;
-                    event.locals.client.user = rows[0].impersonate_user;
-                    event.locals.client.impersonated_by = rows[0].user;
-                }
-                event.locals.client.spaces = rows[0].spaces;
-                event.locals.client.current_space = rows[0].current_space;
-            }
-        } catch (e) {
-            console.log(e);
-        }
+                    spaces.id        AS id,
+                    spaces.slug      AS slug,
+                    spaces.title     AS title,
+                    space_users.role AS role
+                FROM auth.space_users
+                INNER JOIN auth.spaces
+                       ON space_users.space_id=spaces.id
+                WHERE
+                    space_users.user_id=${event.locals.client.user.id}
+            ),
+            _current_space AS (
+                SELECT *
+                FROM _spaces
+                WHERE _spaces.slug=${event.params?.space_slug || null}
+                LIMIT 1
+            )
+            SELECT
+                (SELECT ARRAY_AGG(ROW_TO_JSON(_spaces)) FROM _spaces)::JSONB[] AS spaces,
+                (SELECT ROW_TO_JSON(_current_space) FROM _current_space) AS current_space
+        `)[0];
+
+        event.locals.client.spaces = result.spaces;
+        event.locals.client.current_space = result.current_space;
     }
     if (!event.locals.client.user) event.cookies.delete("session");
 
