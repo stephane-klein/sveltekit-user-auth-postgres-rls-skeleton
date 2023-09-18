444025b92e306019af1aecdaaa41278219c2bf1d
Stéphane Klein <contact@stephane-klein.info>
2023-09-18 17:22:17 +0200

    WIP

--- src/routes/(private)/+layout.svelte
+++ src/routes/(private)/+layout.svelte
@@ -1,8 +1,35 @@
 <script>
+    import { page } from "$app/stores";
+
     export let data;
 </script>
 
 {#if data?.user}
+    <p>
+        Connected as <strong>{data.user.username}</strong>
+        {#if data.current_space}
+            , who has
+            <strong>{data.current_space.role}</strong>
+            role on
+            <a href={`/spaces/${data.current_space.slug}/`}>{data.current_space.title}</a>
+            {#if data?.impersonated}
+                (Impersonated by
+                {data?.impersonated_by?.username}
+                |
+                <a href={`/impersonate/quit/?redirect=${$page.url}`}>Quit impersonate</a>
+                )
+            {/if}
+        {/if}
+        |
+        <a data-sveltekit-reload href="/spaces/">Spaces</a>
+        |
+        <a data-sveltekit-reload href="/audit-events/">Audit events</a>
+        |
+        <a data-sveltekit-reload href="/logout/">Logout</a>
+    </p>
+
+    <hr />
+
     <slot />
 {:else}
     <p>This page does not exist or you are not authorized to access it.</p>
